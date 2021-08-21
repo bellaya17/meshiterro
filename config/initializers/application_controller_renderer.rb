88ee2c0df5039770ab547cf9caf16582d053601c
bellaya17 <belltreeayaka@gmail.com>
2021-08-21 11:27:13 +0000

    [Finish]4-5

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'c1687d6429246bd27b77bb32e031d788bcbd5c2fc988f68604a912b1886ed799f7d04737ed5300131ea58df35885f52ce37892bc38bbb9b646004d52a1369ef4'
